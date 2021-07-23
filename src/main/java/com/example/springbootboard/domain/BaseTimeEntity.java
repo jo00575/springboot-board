94d6ec80e88faa357a20af724385b64813764c43
혜영 <johye0@sk.com>
2021-07-23 16:34:52 +0900

    03. 스프링부트에서 JPA로 데이터베이스 다뤄보기 + JPA Auditing

---
+++ src/main/java/com/example/springbootboard/domain/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package com.example.springbootboard.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass   // JPA Entity 가 BaseTimeEntity 를 상속할 경우 아래 필드들도 컬럼으로 인식하도록 함.
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
---
+++ src/main/java/com/example/springbootboard/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.example.springbootboard.web;
+
+import com.example.springbootboard.service.posts.PostsService;
+import com.example.springbootboard.web.dto.PostsResponseDto;
+import com.example.springbootboard.web.dto.PostsSaveRequestsDto;
+import com.example.springbootboard.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestsDto requestsDto) {
+        return postsService.save(requestsDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
